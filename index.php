09f738331b4c024a211013cce7fdd8d237392ae2
PatoFerrari3C <ferrari.patricio@yahoo.com.ar>
2016-08-16 20:25:48 -0300

    Prueba del commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<title>Ferrari</title>
+</head>
+<body>
+		<!-- Lo que nosotros queremos que se muestre -->
+
+		<h1>Hola Mundo!</h1>
+
+		<?php
+			// echo "Hola PHP";
+
+			// echo "<h2> Hola PHP</h2>"; 
+
+			$numero = 10;
+
+			// echo "El número es ".$numero; // (Forma correcta)
+			 echo "El numero es $numero";
+			// echo 'El número es $numero'; (Asi no va)
+		?>
+</body>
+</html>
